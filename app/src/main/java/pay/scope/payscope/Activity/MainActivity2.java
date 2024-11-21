a764382205cc25b04d59180a782160b3c91f446a
ANSHU SINGH <23anshusingh2003@gmail.com>
2024-11-22 01:09:49 +0530

    Payscope Project

---
+++ app/src/main/java/pay/scope/payscope/Activity/MainActivity2.java
@@ -0,0 +1,19 @@
+package pay.scope.payscope.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import pay.scope.payscope.R;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main2);
+
+    }
+}
